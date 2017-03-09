a6ec95247b91f9e7c79eb0ee0d71f521cf206003
Menuka <menuka.14@cse.mrt.ac.lk>
2017-03-09 12:22:29 +0530

    Add AuthController

---
+++ src/biometricauth/controllers/AuthController.java
@@ -0,0 +1,11 @@
+package biometricauth.controllers;
+
+/**
+ *
+ * @author Max
+ */
+public class AuthController {
+    public static void matchHandGeometry(){
+        
+    }
+}
--- src/biometricauth/controllers/UserController.java
+++ src/biometricauth/controllers/UserController.java
@@ -119,6 +119,7 @@ public class UserController {
                 
             }
         } catch (Exception e) {
+            e.printStackTrace();
         }
         return users;
     }
